ef96132679f07de773989f7aa2f28779c8c6f3d8
Steven Luu <steven@luu.io>
2014-11-06 11:28:05 -0800

    Use sunrise theme

---
+++ SabbathText.Web/Content/sunrise/main.config.js
@@ -0,0 +1,20 @@
+var config = {
+
+    subscription_form_tooltips: {
+
+        /* On successful subscription */
+        success: "You have been subscribed!",
+
+        /* On some unknown error */
+        default_error: "Error! Please, contact administration.",
+
+        /* When email field is empty */
+        empty_email: "Please, enter your phone number.",
+
+        /* When email is invalid (for example, there is no @ character in it) */
+        invalid_email: "Please, enter valid phone number.",
+
+        /* When submitted email is already on the list */
+        already_subscribed: "You are already subscribed."
+    }
+}
