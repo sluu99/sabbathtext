1eb770336387025fb4a4d1d0c16b34c931ed27f2
Steven Luu <steven@luu.io>
2014-11-06 12:26:55 -0800

    Fix view to not display the hand-phone

---
+++ SabbathText.Web/Controllers/GetStartedController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SabbathText.Web.Controllers
+{
+    public class GetStartedController : Controller
+    {
+        public ActionResult Index()
+        {
+
+        }
+    }
+}
