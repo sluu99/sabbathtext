a07355a980c739394ea7941ff9be8f0a4b785755
sluu99 <sluu99@gmail.com>
2015-04-16 22:58:28 -0700

    Removed using statements

--- SabbathText.Location/SunCalculator.cs
+++ SabbathText.Location/SunCalculator.cs
@@ -1,10 +1,6 @@
 ﻿namespace SabbathText.Location
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     /// <summary>
     /// This class provides a way to calculate Sun times.
