147161888b76e2c5b78374c8b1efea73bc302415
sluu99 <sluu99@gmail.com>
2015-04-16 21:39:06 -0700

    Added Location project

---
+++ SabbathText.Location/SunCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace SabbathText.Location
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This class provides a way to calculate Sun times
+    /// </summary>
+    public class SunCalculator
+    {
+    }
+}
