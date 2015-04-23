9f6cfbb89665ce22b31ce2014de2bde15c4e2bca
sluu99 <sluu99@gmail.com>
2015-04-22 23:07:42 -0700

    Added test cases for ZIP messages

--- SabbathText.Tests/V1/ZipMessageTests.cs
+++ SabbathText.Tests/V1/ZipMessageTests.cs
@@ -29,5 +29,44 @@
             AssertLastSentMessage(account.AccountId, MessageTemplate.ZipCodeUpdated, mustContain: "Chicago");
             AssertMessageCount(account.PhoneNumber, MessageTemplate.ZipCodeUpdated, 1);
         }
+
+        /// <summary>
+        /// Tests a ZIP message with a non-existing ZIP code.
+        /// </summary>
+        [TestMethod]
+        public void ZipMessage_LocationNotFound()
+        {
+            const string ZipCode = "12346";
+            AccountEntity account = CreateAccount();
+
+            Message subscribeMessage = this.CreateIncomingMessage(account.PhoneNumber, "subscribe!!");
+            this.ProcessMessage(account, subscribeMessage);
+
+            Message zipMessage = this.CreateIncomingMessage(account.PhoneNumber, "Zip " + ZipCode + ".");
+            this.ProcessMessage(account, zipMessage);
+
+            AssertZipCode(account.AccountId, null);
+            AssertLastSentMessage(account.AccountId, MessageTemplate.LocationNotFound);
+            AssertMessageCount(account.PhoneNumber, MessageTemplate.LocationNotFound, 1);
+        }
+
+        /// <summary>
+        /// Tests an invalid ZIP message.
+        /// </summary>
+        [TestMethod]
+        public void ZipMessage_InvalidMessage()
+        {
+            AccountEntity account = CreateAccount();
+
+            Message subscribeMessage = this.CreateIncomingMessage(account.PhoneNumber, "subscribe");
+            this.ProcessMessage(account, subscribeMessage);
+
+            Message zipMessage = this.CreateIncomingMessage(account.PhoneNumber, "Zip blahblahblah!!!");
+            this.ProcessMessage(account, zipMessage);
+
+            AssertZipCode(account.AccountId, null);
+            AssertLastSentMessage(account.AccountId, MessageTemplate.UpdateZipInstruction);
+            AssertMessageCount(account.PhoneNumber, MessageTemplate.UpdateZipInstruction, 1);
+        }
     }
 }
