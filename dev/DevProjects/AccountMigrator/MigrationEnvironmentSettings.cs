3a248c724320afdda088f03f16695e24a1510d44
sluu99 <sluu99@gmail.com>
2015-05-24 13:31:41 -0700

    Added AccountMigrator project

---
+++ dev/DevProjects/AccountMigrator/MigrationEnvironmentSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SabbathText;
+
+namespace AccountMigrator
+{
+    public class MigrationEnvironmentSettings : DevEnvironmentSettings
+    {
+        public override string KeyValueStoreConnectionString
+        {
+            get
+            {
+                return "UseDevelopmentStorage=true";
+            }
+        }
+    }
+}
