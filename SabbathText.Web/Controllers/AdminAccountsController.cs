3919fcdd0f2e2379543b75606fe48a22a24629bb
sluu99 <sluu99@gmail.com>
2015-05-24 23:43:07 -0700

    Added admin page

---
+++ SabbathText.Web/Controllers/AdminAccountsController.cs
@@ -0,0 +1,23 @@
+﻿namespace SabbathText.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// The controller for administrating accounts
+    /// </summary>
+    public class AdminAccountsController : BaseController
+    {
+        /// <summary>
+        /// Home page for managing accounts
+        /// </summary>
+        /// <returns>The action result</returns>
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
