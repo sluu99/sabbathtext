6400d17ef1aa599aae141932cd19484032d42aab
sluu99 <sluu99@gmail.com>
2015-06-20 13:00:04 -0700

    Fixed bugs in TracingTelemetryTracker

--- SabbathText/Telemetry/TracingTelemetryTracker.cs
+++ SabbathText/Telemetry/TracingTelemetryTracker.cs
@@ -20,7 +20,7 @@
         protected override void TrackEvent(string eventName, IDictionary<string, string> properties = null, IDictionary<string, double> metrics = null)
         {
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine("Real clock: {0}. Virtual clock (UTC): {1}".InvariantFormat(DateTime.Now, Clock.UtcNow));
+            sb.AppendLine("Real clock (UTC): {0}. Virtual clock (UTC): {1}".InvariantFormat(DateTime.Now.ToUniversalTime(), Clock.UtcNow));
             sb.AppendLine("Event: {0}".InvariantFormat(eventName));
 
             if (properties != null)
@@ -30,7 +30,7 @@
 
             if (metrics != null)
             {
-                sb.AppendLine(JsonConvert.SerializeObject(properties, Formatting.Indented));
+                sb.AppendLine(JsonConvert.SerializeObject(metrics, Formatting.Indented));
             }
 
             Trace.TraceInformation(sb.ToString());
@@ -45,7 +45,7 @@
         protected override void TrackException(Exception exception, IDictionary<string, string> properties = null, IDictionary<string, double> metrics = null)
         {
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine("Real clock: {0}. Virtual clock (UTC): {1}".InvariantFormat(DateTime.Now, Clock.UtcNow));
+            sb.AppendLine("Real clock: {0}. Virtual clock (UTC): {1}".InvariantFormat(DateTime.Now.ToUniversalTime(), Clock.UtcNow));
             sb.AppendLine("Exception: {0}".InvariantFormat(exception.Message));
             sb.AppendLine(exception.StackTrace);
 
@@ -56,7 +56,7 @@
 
             if (metrics != null)
             {
-                sb.AppendLine(JsonConvert.SerializeObject(properties, Formatting.Indented));
+                sb.AppendLine(JsonConvert.SerializeObject(metrics, Formatting.Indented));
             }
 
             Trace.TraceError(sb.ToString());
